ae6a0897fb19d39a92769cb2464d2478834ae263
riohib <riyasat.ohib@gmail.com>
2020-09-22 15:57:48 -0400

    updated

--- CNN_LeNet_5/utils/var_projection.py
+++ CNN_LeNet_5/utils/var_projection.py
@@ -9,24 +9,21 @@ import time
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
 def sparsity(matrix):
-    ni = matrix.shape[0]
+    r = len(matrix)  # no of vectors
+    spx = 0
+    spxList = []
 
-    zero_col_ind = (matrix.sum(0) == 0).nonzero().view(-1)  # Get Indices of all zero vector columns.
-
-    # pdb.set_trace()
-
-    spx_c = (np.sqrt(ni) - torch.norm(matrix,1, dim=0) / torch.norm(matrix,2, dim=0)) / (np.sqrt(ni) - 1)
-
-    if len(zero_col_ind) != 0:
-        spx_c[zero_col_ind] = 1  # Sparsity = 1 if column already zero vector.
-
-    # pdb.set_trace()
-    sps_avg =  spx_c.sum() / matrix.shape[1]
-
-    return sps_avg
-
+    for i in range(r):
+        if matrix[i].sum() == 0:
+            spx = 1
+            spxList.append(spx)
+        else:
+            ni = matrix[i].shape[0]
+            spx = (np.sqrt(ni) - torch.norm(matrix[i], 1) / torch.norm(matrix[i], 2)) / (np.sqrt(ni) - 1)
+            spxList.append(spx)
+        spx = sum(spxList) / r
+    return spx
 
 def checkCritical(vector, critval_list, precision=1e-6):
     max_element = max(vector).item()
@@ -39,32 +36,32 @@ def checkCritical(vector, critval_list, precision=1e-6):
     return critval_list, max_element
 
 
-def gmu(matrix, xp_vec, mu=0):
+def gmu(matrix, xp_mat, mu=0):
     vgmu = 0
     gradg = 0
-    matrix = torch.abs(matrix)
+    # matrix = torch.abs(matrix)
     glist = []
 
     gsp_iter = 0
-    for i in range(matrix.shape[1]):
-        ni = matrix[:, i].shape[0]
+    for i in range(len(matrix)):
+        ni = matrix[i].shape[0]
         betai = 1 / (torch.sqrt(torch.tensor(ni, dtype=torch.float32, device=device)) - 1)
 
-        xp_vec[:, i] = matrix[:, i] - (mu * betai)  # .to(device)
-        indtp = torch.where(xp_vec[:, i] > 0)[0]
+        xp_mat[i] = matrix[i] - (mu * betai)  # .to(device)
+        indtp = torch.where(xp_mat[i] > 0)[0]
 
-        xp_vec[:, i] = torch.relu(xp_vec[:, i])
+        xp_mat[i] = torch.relu(xp_mat[i])
 
         # Save xp_vec:
         gsp_iter += 1
 
         # Outputs
-        f2 = torch.norm(xp_vec[:, i])
+        f2 = torch.norm(xp_mat[i])
         if f2 > 0:
             nip = len(indtp)  # may be needs change here
             ev = torch.ones((nip, 1), device=device)
 
-            term2 = torch.pow(torch.matmul(ev.T, xp_vec[indtp, i]), 2)
+            term2 = torch.pow(torch.matmul(ev.T, xp_mat[i][indtp]), 2)
             # new_grad = torch.pow(betai, 2) * (-nip * torch.pow(f2, -1) + term2 * torch.pow(f2, -3))
             # gradg = gradg + new_grad
             # glist.append(new_grad.item())
@@ -74,25 +71,23 @@ def gmu(matrix, xp_vec, mu=0):
             glist.append(new_grad.item())
 
         if indtp.numel() != 0:
-            vec_norm = torch.norm(xp_vec[:, i])
+            xp_mat[i] = xp_mat[i]/torch.norm(xp_mat[i])
 
-            if vec_norm == 0:
-                scipy.io.savemat('norm_zero.mat', mdict={'arr': xp_vec})
+            # if vec_norm == 0:
+            #     scipy.io.savemat('norm_zero.mat', mdict={'arr': xp_vec})
 
-            xp_vec[:, i] = xp_vec[:, i] / vec_norm
-            vgmu = vgmu + betai * torch.sum(xp_vec[:, i])
+            vgmu = vgmu + betai * torch.sum(xp_mat[i])
             # glist.append((betai * torch.sum(xp_vec[:, i])).item() )
-
         else:
-            im = torch.argmax(matrix[:, i])
-            xp_vec[im, i] = 1
+            im = torch.argmax(matrix[i])
+            xp_mat[i][im] = 1
             vgmu = vgmu + betai
             # glist.append(betai.item())
 
         # if torch.isnan(xp_vec[:, i]).sum():
         #     pdb.set_trace()
 
-    return vgmu, xp_vec, gradg
+    return vgmu, xp_mat, gradg
 
 
 def groupedsparseproj(matrix, sps, precision=1e-6, linrat=0.9):
@@ -101,26 +96,28 @@ def groupedsparseproj(matrix, sps, precision=1e-6, linrat=0.9):
     epsilon = 10e-15
     k = 0
     muup0 = 0
-    r = matrix.shape[1]  # No of Columns
+    r = len(matrix)  # No of Columns
 
     critmu = torch.tensor([])
     critval_list = []
 
     vgmu = torch.zeros(1, device=device)
     # maxxi_list = []
-
-    # These operations were inside the loop, but doesn't need to be.
-    matrix_sign = torch.sign(matrix)
-    pos_matrix = matrix_sign * matrix
-    xp_vec = torch.zeros([matrix.shape[0], matrix.shape[1]]).to(device)
-    ni = matrix.shape[0]
+    sx = {}
+    pos_matrix = {}
+    xp_mat = {}
 
     for i in range(r):
+    
+        sx[i] = torch.sign(matrix[i])
+        pos_matrix[i] = sx[i] * matrix[i]
+        ni = matrix[i].shape[0]
+
         k = k + np.sqrt(ni) / (np.sqrt(ni) - 1)
         # check critical values of mu where g(mu) is discontinuous, that is,
         # where the two (or more) largest entries of x{i} are equal to one another.
 
-        critical_val, max_xi = checkCritical(pos_matrix[:, i], critval_list)
+        critical_val, max_xi = checkCritical(pos_matrix[i], critval_list)
 
         # maxxi_list.append(max_xi)
 
@@ -128,10 +125,10 @@ def groupedsparseproj(matrix, sps, precision=1e-6, linrat=0.9):
         critmu = torch.tensor(critval_list) * (np.sqrt(ni) - 1)
 
     k = k - r * sps
-    vgmu, xp_vec, gradg = gmu(pos_matrix, xp_vec, 0)
+    vgmu, xp_mat, gradg = gmu(pos_matrix, xp_mat, 0)
 
     if vgmu < k:
-        xp_mat = matrix
+        xp_mat = pos_matrix
         gxpmu = vgmu
         numiter = 0
         return xp_mat
@@ -161,7 +158,7 @@ def groupedsparseproj(matrix, sps, precision=1e-6, linrat=0.9):
             if (newmu >= muup) or (newmu <= mulow):  # If Newton goes out of the interval, use bisection
                 newmu = (mulow + muup) / 2
 
-            gnew, xnew, gpnew = gmu(matrix, xp_vec, newmu)
+            gnew, xnew, gpnew = gmu(pos_matrix, xp_mat, newmu)
 
             if gnew < k:
                 gup = gnew
@@ -175,7 +172,7 @@ def groupedsparseproj(matrix, sps, precision=1e-6, linrat=0.9):
             # Guarantees linear convergence
             if (muup - mulow) > linrat * delta and abs(oldmu - newmu) < (1 - linrat) * delta:
                 newmu = (mulow + muup) / 2
-                gnew, xnew, gpnew = gmu(matrix, xp_vec, newmu)
+                gnew, xnew, gpnew = gmu(pos_matrix, xp_mat, newmu)
 
                 if gnew < k:
                     gup = gnew
@@ -194,31 +191,48 @@ def groupedsparseproj(matrix, sps, precision=1e-6, linrat=0.9):
                 xp = xnew
                 gxpmu = gnew
         try:
-            xp_vec = xnew
+            xp_mat = xnew
             # print(' xp_vec = xnew')
         except:
             scipy.io.savemat('matrix.mat', mdict={'arr': matrix})
 
         gxpmu = gnew
 
-    # pdb.set_trace()
 
-    alpha = torch.zeros([1, matrix.shape[1]], device=device)
+    # alpha = torch.zeros([1, matrix.shape[1]], device=device)
+    alpha = {}
     for i in range(r):
-        alpha[0, i] = torch.matmul(xp_vec[:, i], pos_matrix[:, i])
-        xp_vec[:, i] = alpha[:, i] * (matrix_sign[:, i] * xp_vec[:, i])
+        alpha[i] = torch.matmul(xp_mat[i], pos_matrix[i])
+        xp_mat[i] = alpha[i] * (sx[i] * xp_mat[i])
 
-    return xp_vec
+    return xp_mat
 
 
-def load_matrix_debug(test_matrix):
-    with open(test_matrix, "rb") as fpA:  # Pickling
-        matrix = pickle.load(fpA)
+def load_matrix_debug(mat_tuple):
+    
+    matrix_1, matrix_2, matrix_3, matrix_4 = mat_tuple
+    with open(matrix_1, "rb") as fpA:  # Pickling
+        matrix_1 = pickle.load(fpA)
+    with open(matrix_2, "rb") as fpA:  # Pickling
+        matrix_2 = pickle.load(fpA)
+    with open(matrix_3, "rb") as fpA:  # Pickling
+        matrix_3 = pickle.load(fpA)
+    with open(matrix_4, "rb") as fpA:  # Pickling
+        matrix_4 = pickle.load(fpA)
+
+    matrix_1 = torch.from_numpy(matrix_1).view(-1)
+    matrix_2 = torch.from_numpy(matrix_2).view(-1)
+    matrix_3 = torch.from_numpy(matrix_3).view(-1)
+    matrix_4 = torch.from_numpy(matrix_4).view(-1)
+
+    matrix = {0:matrix_1, 1:matrix_2, 2:matrix_3, 3:matrix_4}
     return matrix
 
 
 # ## ********************************************************************************** ##
-# matrix = load_matrix_debug()
+# mat_tuple = ("matrix_1.pkl", "matrix_2.pkl", "matrix_3.pkl", "matrix_4.pkl")
+# matrix = load_matrix_debug(mat_tuple)
+
 # start_time = time.time()
 # sps = 0.9
 # precision = 1e-6
